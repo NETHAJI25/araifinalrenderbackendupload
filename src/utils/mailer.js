9ac1be86301c23e5ed101d3238cf813c153b19ff
R. NETHAJI <nethajipurushothaman25@gmail.com>
2026-09-25 12:20:36 +0530

    Add SMTP fail-fast timeouts

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -13,6 +13,9 @@ function getTransporter() {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_APP_PASSWORD,
     },
+    connectionTimeout: 10000, // fail fast instead of hanging
+    greetingTimeout: 10000,
+    socketTimeout: 15000,
   });
   return transporter;
 }
